dc9e34a0daed9b55bc64e1aa04a25166b060b251
Wojciech Zielonka <g36@wp.pl>
2018-05-01 14:20:41 +0200

    Edges cirlces

--- Tracker/main.cpp
+++ Tracker/main.cpp
@@ -26,19 +26,40 @@ void toBinary() {
     adaptiveThreshold(grayFrame, binaryFrame, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 11, 12);
 }
 
+void draw7points(Point a, Point b){
+    Point c = b - a;
+    double l = norm(c) / 7;
+    double s = l;
+
+    c *= 1/norm(c);
+
+    for(int i = 0; i < 7; i++) {
+        circle(binaryFrame, a + (c * s), 1, Scalar(0, 100, 255), 3, 8, 0);
+        s += l;
+    }
+}
+
 void extractShapes() {
-    cv::findContours(binaryFrame, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+    findContours(binaryFrame, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
     for (int i = 0; i < contours.size(); i++)
     {
         vector<Point> approx;
         approxPolyDP(contours[i], approx, 3, true);
         if (approx.size() == 4) {
             Rect rect = boundingRect(Mat(approx));
-            if (rect.area() > 50) {
-                //rectangle(binaryFrame, rect.tl(), rect.br(), Scalar(100, 100, 255), 2, 8, 0);
+            double area = rect.size().area();
+            if (area > 1000) {
                 polylines(binaryFrame, approx, true, Scalar(100, 100, 255), 2, 8, 0);
+
+                int p = 1;
+                for(; p < approx.size(); p++){
+                    draw7points(approx[p - 1], approx[p]);
+                }
+
+                draw7points(approx[p - 1], approx[0]);
+
                 for (int j = 0; j < approx.size(); j++) {
-                    circle(binaryFrame, approx[j], 1, Scalar(100, 100, 255), 3, 8, 0);
+                    circle(binaryFrame, approx[j], 1, Scalar(100, 0, 255), 3, 8, 0);
                 }
             }
         }
@@ -68,7 +89,7 @@ int main(int argc, const char** argv)
 
         imshow("Threshold", binaryFrame);
 
-        std::this_thread::sleep_for(0.1s);
+        std::this_thread::sleep_for(0.010s);
 
         char key = cvWaitKey(10);
         if (key == 27) break;
