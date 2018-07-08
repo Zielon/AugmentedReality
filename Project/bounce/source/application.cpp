74dbc9e5bc3ec16966cbc6f5f7c625dbcac76ed5
Wojciech Zielonka <g36@wp.pl>
2018-07-08 21:20:52 +0200

    viewport

--- Project/bounce/source/application.cpp
+++ Project/bounce/source/application.cpp
@@ -110,6 +110,8 @@ void Application::display(Mat &mat) {
 void Application::initialize() {
     glClearColor(0.0, 0.0, 0.0, 1.0);
 
+    glViewport(0, 0, WIDTH, HEIGHT);
+
     glEnable(GL_COLOR_MATERIAL);
     glEnable(GL_BLEND);
     glEnable(GL_LINE_SMOOTH);
